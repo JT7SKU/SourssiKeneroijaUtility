1589fb4249afdb9fc4a8a888ef6e2f0b30fb6927
Janisku7 <janisku7@gmail.com>
2026-06-12 16:54:10 +0300

    testing ways

---
+++ SourssiKeneroijaUtility.EnumIntegrointiTestit/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace SourssiKeneroijaUtility.EnumIntegrointiTestit
+{
+    public class UnitTest1
+    {
+
+        //[Theory]
+        //[InlineData(1)]
+        //public void FastToStringIsSameAsToString()
+        //{
+        //    var expected = "" ;
+        //    var actual = "";
+
+        //    Assert.Equal(expected, actual);
+        //}
+    }
+}
